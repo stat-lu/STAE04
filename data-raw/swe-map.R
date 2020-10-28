bdcde46c2159a8b3c578a52f6fb5db4ab8a42960
Johan Larsson <johanlarsson@outlook.com>
2020-10-28 13:27:39 +0100

    unify region code formatting

--- data-raw/swe-map.R
+++ data-raw/swe-map.R
@@ -11,6 +11,7 @@ tmp <-
          municipalty_name = knnamn,
          group,
          lon = leaflet_long,
-         lat = leaflet_lat)
+         lat = leaflet_lat) %>%
+  mutate(region_code = str_trunc(region_code, 2, "left", ""))
 
 write_csv(tmp, "data/swe-map.csv")
